3ee1faccd228243579fc1b78a7a229f69ce15714
huangshuai <chimmhuang@163.com>
2020-10-13 14:26:56 +0800

    feat: 新增动态添加/删除表格的测试

--- src/test/java/com/github/chimmhuang/demo/Demo.java
+++ src/test/java/com/github/chimmhuang/demo/Demo.java
@@ -17,6 +17,9 @@ import java.io.FileOutputStream;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 /**
  * @author Chimm Huang
@@ -25,16 +28,23 @@ public class Demo {
 
     @Test
     public void testFillInTable() throws Exception {
-        File file = new File("src/test/resources/demo.xlsx");
 
+        // 获取 excel 二进制文件
+        File file = new File("src/test/resources/demo.xlsx");
         byte[] bytes = FileUtils.readFileToByteArray(file);
 
+        // 创建 table 对象
         ExcelWorkbook excelWorkbook = ExcelHelper.createWorkbook(bytes);
         SheetTable table = excelWorkbook.getSheet(0);
 
-        // define table data
+        // 定义 excel 表格数据
         SchoolReportData tableData = new SchoolReportData();
 
+        /*
+            ====================================
+                       开始组装表格数据
+            ====================================
+         */
         tableData.setTitle("中和中学成绩单");
 
         GradesRanking gradesRanking = new GradesRanking();
@@ -54,34 +64,62 @@ public class Demo {
 
 
         List<ClassScore> classScoreList = Arrays.asList(
-                new ClassScore("高一1班", "李华", BigDecimal.valueOf(285), BigDecimal.valueOf(90), BigDecimal.valueOf(95), BigDecimal.valueOf(100)),
-                new ClassScore("高一1班", "小明", BigDecimal.valueOf(270), BigDecimal.valueOf(80), BigDecimal.valueOf(100), BigDecimal.valueOf(90)),
-                new ClassScore("高一1班", "张三", BigDecimal.valueOf(265), BigDecimal.valueOf(80), BigDecimal.valueOf(95), BigDecimal.valueOf(90)),
-                new ClassScore("高一2班", "小红", BigDecimal.valueOf(260), BigDecimal.valueOf(95), BigDecimal.valueOf(70), BigDecimal.valueOf(95)),
-                new ClassScore("高一2班", "马小跳", BigDecimal.valueOf(200), BigDecimal.valueOf(60), BigDecimal.valueOf(60), BigDecimal.valueOf(80)),
-                new ClassScore("高一2班", "李三光", BigDecimal.valueOf(255), BigDecimal.valueOf(55), BigDecimal.valueOf(100), BigDecimal.valueOf(100)),
-                new ClassScore("高一2班", "李四", BigDecimal.valueOf(250), BigDecimal.valueOf(80), BigDecimal.valueOf(90), BigDecimal.valueOf(80)));
+                new ClassScore(1,"高一1班", "李华", BigDecimal.valueOf(285), BigDecimal.valueOf(90), BigDecimal.valueOf(95), BigDecimal.valueOf(100)),
+                new ClassScore(1,"高一1班", "小明", BigDecimal.valueOf(270), BigDecimal.valueOf(80), BigDecimal.valueOf(100), BigDecimal.valueOf(90)),
+                new ClassScore(1,"高一1班", "张三", BigDecimal.valueOf(265), BigDecimal.valueOf(80), BigDecimal.valueOf(95), BigDecimal.valueOf(90)),
+                new ClassScore(2,"高一2班", "小红", BigDecimal.valueOf(260), BigDecimal.valueOf(95), BigDecimal.valueOf(70), BigDecimal.valueOf(95)),
+                new ClassScore(2,"高一2班", "马小跳", BigDecimal.valueOf(200), BigDecimal.valueOf(60), BigDecimal.valueOf(60), BigDecimal.valueOf(80)),
+                new ClassScore(2,"高一2班", "李三光", BigDecimal.valueOf(255), BigDecimal.valueOf(55), BigDecimal.valueOf(100), BigDecimal.valueOf(100)),
+                new ClassScore(2,"高一2班", "李四", BigDecimal.valueOf(250), BigDecimal.valueOf(80), BigDecimal.valueOf(90), BigDecimal.valueOf(80)));
         tableData.setClassScoreList(classScoreList);
 
         tableData.setPrincipalComment("允德允能");
 
+        /*
+            ====================================
+                       表格数据组装完毕
+                       -------------
+                       开始动态设置表格
+            ====================================
+         */
+
         Row row13 = table.getRow(13);
         Row row14 = table.getRow(14);
-
+        // 将 rowNum 大于 13 的都删除，进行动态表格添加
         table.removeRowGE(13);
 
-        Row row15 = table.appendRow(row13);
-        row15.getCell("C").setValue("你好");
+        // 将班级成绩单列表按照 班级id 进行分组遍历
+        int classScoreIndex = 0;
+        Map<Integer, List<ClassScore>> classScoreMap = tableData.getClassScoreList().stream()
+                .collect(Collectors.groupingBy(ClassScore::getClassId));
 
-        Row row16 = table.appendRow(row13);
-        row16.getCell("C").setValue("我不好");
+        for (Entry<Integer, List<ClassScore>> entry : classScoreMap.entrySet()) {
+            Integer classId = entry.getKey();
+            for (ClassScore classScore : entry.getValue()) {
+                // 复制模板的第13行
+                Row copy = row13.copy();
+                // 此处直接设置值也行。设置值或者设置变量，建议统一。我在此处设置为变量
+                copy.getCell("A").setValue("${classScoreList["+classScoreIndex+"].className}");
+                copy.getCell("B").setValue("${classScoreList["+classScoreIndex+"].name}");
+                copy.getCell("D").setValue("${classScoreList["+classScoreIndex+"].chineseScore}");
+                copy.getCell("E").setValue("${classScoreList["+classScoreIndex+"].mathScore}");
+                copy.getCell("F").setValue("${classScoreList["+classScoreIndex+"].englishScore}");
 
+                classScoreIndex++;
+
+                // 设置完毕后，添加进表格
+                table.appendRow(copy);
+            }
+        }
+
+        // 班级成绩设置好之后，将最后的校长评语添加进表格
         table.appendRow(row14);
 
+        // 将变量的值填充进表格
         ExcelHelper.fillInData(table, tableData);
 
 
-
+        // 获取转换后的二进制
         byte[] bytes1 = ExcelHelper.convert2Byte(table);
 
         // 获取桌面路径
